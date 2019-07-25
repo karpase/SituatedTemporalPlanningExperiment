dcc1123f41d62b53f02769185b9ea01d0c6f41c6
Erez Karpas <karpase@cogrob-lab-01.iem.technion.ac.il>
2019-07-25 11:16:04 +0300

    scripts

--- parse_rewrite_output.sh
+++ parse_rewrite_output.sh
@@ -9,6 +9,6 @@ expanded=`grep "Nodes Expanded: [0-9]*" $1 | awk '{print $4;}'`
 evaluated=`grep  "Nodes Evaluated: [0-9]*" $1 | awk '{print $4;}'`
 makespan=`tail -1 $1 | awk '{print $1 + substr($(NF-2),2);}'`
 
-echo $solved,$unsolvable,$stime,$generated,$expanded,$evaluated,$makespan
+echo $1,$solved,$unsolvable,$stime,$generated,$expanded,$evaluated,$makespan
 
 

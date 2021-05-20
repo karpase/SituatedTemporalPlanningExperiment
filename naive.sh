8fb9b5245352dee5c94287c4979031c311249a4b
Erez Karpas <karpase@gmail.com>
2021-05-20 08:16:18 +0000

    use actual planning time instead of predicted planning time in naive

--- naive.sh
+++ naive.sh
@@ -25,8 +25,12 @@ python3 adjust_til.py $2 $3 $1 $4.adj_$1
 # Call Planner
 $PLANNER $2 $4.adj_$1 > $4.adj_$1.planner.out
 
+# Extract Planning Time
+stime=`grep "; Time [0-9]*" $4.adj_$1.planner.out | awk '{print $3;}' `
+
+
 # Extract solution and adjust it
-gawk --assign=ta=$1 'BEGIN {found=0;} {if (($2 == "Solution") && ($3 == "Found")) {found=1;} if ((found == 1) && (substr($1,1,1) != ";")) {printf("%f: %s\n", $1+ta, substr($0, index($0,":")+1)) }}'  $4.adj_$1.planner.out > $4.adj_$1.planner.out.adjusted
+gawk --assign=ta=$stime 'BEGIN {found=0;} {if (($2 == "Solution") && ($3 == "Found")) {found=1;} if ((found == 1) && (substr($1,1,1) != ";")) {printf("%f: %s\n", $1+ta, substr($0, index($0,":")+1)) }}'  $4.adj_$1.planner.out > $4.adj_$1.planner.out.adjusted
 
 # Validate adjusted solution against original problem
 $VAL -t 0.001 $2 $3 $4.adj_$1.planner.out.adjusted > $4.adj_$1.val

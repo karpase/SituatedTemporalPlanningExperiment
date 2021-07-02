6f0a70c6b2037876c42555eb6bec6a1b5ddedb88
Erez Karpas <karpase@gmail.com>
2021-07-02 08:54:43 +0300

    Create create_pbs_jobs.sh

---
+++ create_pbs_jobs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# $1 - file with list of commands to run
+# $2 - number of commands per PBS job
+
+rm $1.com.*
+
+shuf $1 > $1.shuf
+split -l $2 $1.shuf $1.com.
+
+for x in $1.com.*; do
+    echo '#!/bin/bash' > $x.pbs;
+    echo '#PBS -N' $x >> $x.pbs;
+    echo '#PBS' -q zeus_all_q >> $x.pbs;
+    echo '#PBS' -l select=1:ncpus=1 >> $x.pbs;
+    echo '#PBS' -l walltime=9:00:00  >> $x.pbs;
+    echo '#PBS' -m abe >> $x.pbs;
+    echo '#PBS' -M  karpase@technion.ac.il >> $x.pbs;
+
+    echo PBS_O_WORKDIR=`pwd`  >> $x.pbs;
+    echo cd $PBS_O_WORKDIR  >> $x.pbs;
+
+    cat $x >> $x.pbs;
+
+done

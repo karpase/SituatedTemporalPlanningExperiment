d0e1b682f4923fa8ed68306f2eec932ba8c01d0f
Erez Karpas <karpase@gmail.com>
2021-07-02 08:58:38 +0300

    Update create_pbs_jobs.sh

--- create_pbs_jobs.sh
+++ create_pbs_jobs.sh
@@ -4,14 +4,14 @@
 rm $1.com.*
 
 shuf $1 > $1.shuf
-split -l 280 $1.shuf $1.com.
+split -l 430 $1.shuf $1.com.
 
 for x in $1.com.*; do
     echo '#!/bin/bash' > $x.pbs;
     echo '#PBS -N' $x >> $x.pbs;
     echo '#PBS' -q zeus_all_q >> $x.pbs;
     echo '#PBS' -l select=1:ncpus=1 >> $x.pbs;
-    echo '#PBS' -l walltime=23:30:00  >> $x.pbs;
+    echo '#PBS' -l walltime=23:55:00  >> $x.pbs;
     echo '#PBS' -m abe >> $x.pbs;
     echo '#PBS' -M  karpase@technion.ac.il >> $x.pbs;
 

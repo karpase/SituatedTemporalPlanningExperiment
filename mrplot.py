0071138f824b6337c2eaa02e79f87acba9653749
Erez Karpas <karpase@gmail.com>
2023-02-23 09:06:27 +0200

    nice violin plot

--- mrplot.py
+++ mrplot.py
@@ -1,5 +1,20 @@
 import pandas as pd
 import seaborn as sns
+import matplotlib
+
+matplotlib.use("pgf")
+matplotlib.rcParams.update({
+    "pgf.texsystem": "pdflatex",
+    'font.family': 'serif',
+    'text.usetex': True,
+    'pgf.rcfonts': False,
+})
+
+
 df = pd.read_csv("mrplot.txt", sep=" ")
-plot=sns.violinplot(x="tu", y="mr", data=df,linewidth=3)
-plot.figure.savefig("mrplot.png")
+plot=sns.violinplot(x=df.tu, y=df.mr, data=df, cut=0, scale="width")
+plot.set(
+    xlabel="$t_u$",
+    ylabel="Fraction of Time Spent on Metareasoning")
+plot.figure.savefig("mrplot.pgf")
+

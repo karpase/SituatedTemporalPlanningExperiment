d28750ef4423b9e931812f1e9765719f5e973d18
Erez Karpas <karpase@gmail.com>
2024-10-10 21:48:32 +0300

    fixed parameters

--- cope_instances.py
+++ cope_instances.py
@@ -91,7 +91,7 @@ configurations = []
 
 
 
-default_cmd_params = "--include-metareasoning-time --multiply-TILs-by 1 --real-to-plan-time-multiplier 1 --calculate-Q-interval 100 --add-weighted-f-value-to-Q -0.000001 --min-probability-failure 0.001 --slack-from-heuristic --forbid-self-overlapping-actions --deadline-aware-open-list IJCAI --ijcai-gamma 1 --ijcai-t_u 100 --icaps-for-n-expansions 100 --time-aware-heuristic 1 --dispatch-frontier-size 1 --subtree-focus-threshold 1 --optimistic-lst-for-dispatch-reasoning"
+default_cmd_params = "--include-metareasoning-time --multiply-TILs-by 1 --real-to-plan-time-multiplier 1 --calculate-Q-interval 100 --add-weighted-f-value-to-Q -0.000001 --min-probability-failure 0.001 --slack-from-heuristic --forbid-self-overlapping-actions --deadline-aware-open-list IJCAI --ijcai-gamma 1 --ijcai-t_u 100 --icaps-for-n-expansions 100 --time-aware-heuristic 1 --dispatch-frontier-size 10 --subtree-focus-threshold 1 --optimistic-lst-for-dispatch-reasoning"
 
 def add_config(configurations, expansions_per_second, dispatch, dispatch_threshold=None):
 	cmd_params = default_cmd_params + " --time-based-on-expansions-per-second " + str(expansions_per_second)

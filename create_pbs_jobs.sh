c3a994617aab2c64f862e5905a9aedff39cb61f5
Erez Karpas <karpase@gmail.com>
2024-10-10 11:18:07 +0300

    spread jobs more evenly, increase timeout

--- create_pbs_jobs.sh
+++ create_pbs_jobs.sh
@@ -4,7 +4,7 @@
 rm $1.com.*
 
 shuf $1 > $1.shuf
-split -l 8000 $1.shuf $1.com.
+split -l 1000 $1.shuf $1.com.
 
 for x in $1.com.*; do
     echo '#!/bin/bash' > $x.pbs;
@@ -18,6 +18,6 @@ for x in $1.com.*; do
     PBS_O_WORKDIR=`pwd`
     echo cd $PBS_O_WORKDIR  >> $x.pbs;
 
-    echo python $PBS_O_WORKDIR/parallel.py --filename $PBS_O_WORKDIR/$x --num_processes 20 --timeout 200 >> $x.pbs
+    echo python $PBS_O_WORKDIR/parallel.py --filename $PBS_O_WORKDIR/$x --num_processes 20 --timeout 600 >> $x.pbs
 
 done
